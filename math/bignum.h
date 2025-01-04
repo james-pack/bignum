1f3e58ecf01c386cbb3f561a54467799c0f699a3
James Pack <jgrimm@gmail.com>
2025-01-04 01:05:13 -0500

    Added tests and fixed bug in addition

--- math/bignum.h
+++ math/bignum.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <cstdint>
+#include <string>
 #include <vector>
 
 namespace jp::math {
@@ -24,30 +25,42 @@ class BigNum final {
   BigNum& operator=(const BigNum&) = default;
   BigNum& operator=(BigNum&&) = default;
 
-  BigNum(int i) : value_(1, i) {}
-  BigNum(unsigned long i) : value_(1, i) {}
+  BigNum(uint64_t v) : value_(1, v) {}
+
+  const StoreT& operator[](size_t index) const { return value_.at(value_.size() - index - 1); }
+  StoreT& operator[](size_t index) { return value_.at(value_.size() - index - 1); }
+
+  size_t size() const { return value_.size(); }
 
   BigNum operator+(const BigNum& rhs) const {
     BigNum result{rhs};
-    result.upsize(value_.size());
+    result.upsize(size());
 
     StoreT carry{0};
-    for (size_t i = 0; i < value_.size(); ++i) {
-      result.value_[i] += value_[i] + carry;
-      if (result.value_[i] < value_[i]) {
-        carry = 1;
-      }
+    for (size_t i = 0; i < size(); ++i) {
+      carry = __builtin_add_overflow(result[i], carry, &result[i]);
+      carry += __builtin_add_overflow(result[i], value_[i], &result[i]);
     }
 
     if (carry > 0) {
-      result.upsize(result.value_.size() + 1);
-      result.value_[result.value_.size() - 1] = 1;
+      result.upsize(result.size() + 1);
+      result[result.size() - 1] = 1;
     }
 
     return result;
   }
-
-  const StoreT& operator[](size_t i) const { return value_.at(value_.size() - i - 1); }
 };
+
+std::string to_string(const BigNum& n) {
+  // TODO(james): This is a quick and dirty form of this function for debug info. Either remove or
+  // fix.
+  using std::to_string;
+  std::string result{};
+  for (size_t i = 0; i < n.size(); ++i) {
+    result.append(to_string(n[n.size() - i - 1]));
+    result.append(" ");
+  }
+  return result;
+}
 
 }  // namespace jp::math
--- math/bignum_test.cc
+++ math/bignum_test.cc
@@ -4,9 +4,22 @@
 
 namespace jp::math {
 
-TEST(BigNumTest, CanCompile) {
+TEST(BigNumTest, CanInstantiate) {
   BigNum zero{};
   EXPECT_EQ(0, zero[0]);
+  BigNum one{1};
+  EXPECT_EQ(1, one[0]);
+  BigNum fifteen{15};
+  EXPECT_EQ(15, fifteen[0]);
+  BigNum max{std::numeric_limits<uint64_t>::max()};
+  EXPECT_EQ(std::numeric_limits<uint64_t>::max(), max[0]);
+}
+
+TEST(BigNumTest, CanAdd) {
+  BigNum zero{};
+  BigNum one{1};
+  BigNum sum{zero + one};
+  EXPECT_EQ(1, sum[0]);
 }
 
 }  // namespace jp::math

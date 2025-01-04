35787c9e1c4d6579a0a10fd36991fe59a3db7f58
James Pack <jgrimm@gmail.com>
2025-01-04 01:24:17 -0500

    Modified main to time computation of fib for large-ish n

--- math/fib_main.cc
+++ math/fib_main.cc
@@ -6,11 +6,11 @@
 using namespace jp::math;
 
 int main() {
-  for (uint64_t n = 0; n < 100; ++n) {
-    BigNum result{fib(n)};
-    std::cout << to_string(n) << ": " << to_string(result) << "\n";
-  }
+  // for (uint64_t n = 0; n < 100; ++n) {
+  //   BigNum result{fib(n)};
+  //   std::cout << to_string(n) << ": " << to_string(result) << "\n";
+  // }
 
-  // volatile BigNum result{fib(3)};
+  volatile BigNum result{fib(400'000)};
   return 0;
 }
